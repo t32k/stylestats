09a48f492dcb75ed4250c0d730947a4dbe18dbf5
1000ch <shogo.sensui@gmail.com>
2015-01-04 01:06:28 +0900

    format indent

--- test/util-test.js
+++ test/util-test.js
@@ -2,16 +2,16 @@ var assert = require('assert');
 var util = require('../lib/util.js');
 
 describe('Utility functions', function() {
-    it('should return false', function() {
-        var result = util.isFile('test/fixture/not_exist.css');
-        assert.equal(result, false);
-    });
-    it('should return false', function() {
-        var result = util.isDirectory('test/fixture/not_exist/');
-        assert.equal(result, false);
-    });
-    it('should return false', function() {
-        var result = util.isCSS('not.css');
-        assert.equal(result, false);
-    });
+  it('should return false', function() {
+    var result = util.isFile('test/fixture/not_exist.css');
+    assert.equal(result, false);
+  });
+  it('should return false', function() {
+    var result = util.isDirectory('test/fixture/not_exist/');
+    assert.equal(result, false);
+  });
+  it('should return false', function() {
+    var result = util.isCSS('not.css');
+    assert.equal(result, false);
+  });
 });
