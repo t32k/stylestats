12a508087affc2a8943fe6c0cc535fa913579b7e
t32k <ijok.ijok@gmail.com>
2015-04-20 08:15:44 -0700

    Fix unnesesary if

--- bin/cli.js
+++ bin/cli.js
@@ -35,16 +35,10 @@ var config = {
     headers: {}
   }
 };
-
-if (program.gzip) {
-  config.gzippedSize = true;
-}
-
 if (program.mobile) {
   var iOS = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A365 Safari/600.1.4';
   config.requestOptions.headers['User-Agent'] = iOS;
 }
-
 if (program.number) {
   var numberConfig = {
     "published": false,
