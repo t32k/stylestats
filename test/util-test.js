843ea935dcbfb5b3fc01aff71d3d6700b0200eb1
t32k <ijok.ijok@gmail.com>
2015-04-20 19:58:19 -0700

    Refactor prittify.js and add test

--- test/util-test.js
+++ test/util-test.js
@@ -1,16 +1,18 @@
+var fs = require('fs');
 var assert = require('assert');
 var util = require('../lib/util.js');
+var prettify = require('../lib/prettify.js');
 
-describe('Utility functions', function() {
+describe('Utility functions', function () {
 
   describe('isFile()', function () {
 
-    it('should return true if specified path is a file', function() {
+    it('should return true if specified path is a file', function () {
       var result = util.isFile('test/fixture/app.css');
       assert.equal(result, true);
     });
 
-    it('should return false if specified path is not a file', function() {
+    it('should return false if specified path is not a file', function () {
       var result = util.isFile('test/fixture/not_exist.css');
       assert.equal(result, false);
     });
@@ -18,12 +20,12 @@ describe('Utility functions', function() {
 
   describe('isDirectory()', function () {
 
-    it('should return true if specified path is a directory', function() {
+    it('should return true if specified path is a directory', function () {
       var result = util.isDirectory('test/fixture/');
       assert.equal(result, true);
     });
 
-    it('should return false if specified path is not a directory', function() {
+    it('should return false if specified path is not a directory', function () {
       var result = util.isDirectory('test/fixture/not_exist/');
       assert.equal(result, false);
     });
@@ -31,14 +33,39 @@ describe('Utility functions', function() {
 
   describe('isCSS()', function () {
 
-    it('should return true if specified path is CSS string', function() {
+    it('should return true if specified path is CSS string', function () {
       var result = util.isCSS('.foo{}');
       assert.equal(result, true);
     });
 
-    it('should return false if specified path is not CSS string', function() {
+    it('should return false if specified path is not CSS string', function () {
       var result = util.isCSS('.bar{');
       assert.equal(result, false);
+    });
+  });
+});
+
+
+describe('Unit tests', function () {
+  var json = fs.readFileSync('test/fixture/result.json', {
+    encoding: 'utf8'
+  });
+  var data = prettify(JSON.parse(json));
+  describe('prettify()', function () {
+    it('should return "Style Sheets" key and value', function () {
+      assert.deepEqual(data[0], {'Style Sheets': 2});
+    });
+    it('should return "Gzipped Size" key and value', function () {
+      assert.deepEqual(data[3], {'Gzipped Size': '2.0KB'});
+    });
+    it('should return "Simplicity" key and value', function () {
+      assert.deepEqual(data[6], {Simplicity: '49.2%'});
+    });
+    it('should return "Unique Colors" key and value', function () {
+      assert.deepEqual(data[18], {'Unique Colors': '#0FA0CE\n#1EAEDB\n#222222\n#333333\n#555555\n#FFFFFF'});
+    });
+    it('should return "Properties Count" key and value', function () {
+      assert.deepEqual(data[25], {'Properties Count': 'width: 20\nmargin-left: 18\nfont-size: 17\nline-height: 9\npadding: 9\nmargin-bottom: 8\nletter-spacing: 7\ncolor: 7\nbox-sizing: 6\ndisplay: 6'});
     });
   });
 });
