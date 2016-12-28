3306b5375802584f2eb34f0936fb643b18976e44
t32k <ijok.ijok@gmail.com>
2016-12-28 22:57:59 +0900

    Update UA

--- bin/cli.js
+++ bin/cli.js
@@ -33,7 +33,7 @@ const config = {
     headers: {}
   }
 };
-const MOBILE_UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1';
+const MOBILE_UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A5297c Safari/602.1';
 const numberConfig = {
   "published": false,
   "paths": false,
